3ca0cf03c177030c3eaacec84372d020d6d1b567
micinfo <michael.alumno@gmail.com>
2016-12-18 20:13:33 +0800

    contact us content

--- js/contact.js
+++ js/contact.js
@@ -8,8 +8,26 @@ const Contact = React.createClass({
 
     render() {
         return(
-            <div>
-                Contact
+            <div className="section">
+            	<div className="container">
+            		<center>
+                        <img className="responsive-img" src="img/contactUs.png" />
+                    </center>
+                    <div className="section">
+                        <p style={{textAlign : 'center'}}>
+                        	<i className="material-icons dp48" style={{verticalAlign: 'bottom'}}>my_location</i>&nbsp;
+                        	115 Kasing-Kasing corner K-16th St. <br/> Brgy. East Kamias, Quezon City
+                        </p> 
+                        <p style={{textAlign : 'center'}}>
+                        	<i className="material-icons dp48" style={{verticalAlign: 'bottom'}}>phone</i>&nbsp;
+                        	435-6566 / 435-4535 / 433-7790 / 434-6583
+                        </p>
+                        <p style={{textAlign : 'center'}}>
+                        	<i className="material-icons dp48" style={{verticalAlign: 'bottom'}}>email</i>&nbsp;
+                        	info@activationsadvertising.com
+                        </p>
+                    </div>
+            	</div>
             </div>
         )
     }
