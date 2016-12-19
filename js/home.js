65d74b25ed988d3e5f3fb622eec81bb6b21d5496
micinfo <michael.alumno@gmail.com>
2016-12-19 18:00:36 +0800

    fix mobile view home page

--- js/home.js
+++ js/home.js
@@ -8,7 +8,13 @@ const Home = React.createClass({
                 {
                     captionClass: 'col s12 l4 right center-align',
                     image: 'img/home/activations.jpg',
+                    imageMobile : 'img/aai_logo.png',
                     title: [
+                        <span> Advertising that go </span>, 
+                        <span className="orange-text text-accent-3"> Beyond </span>, 
+                        <span> Expectations </span>
+                    ],
+                    titleMobile: [
                         <span> Advertising that go </span>, 
                         <span className="orange-text text-accent-3"> Beyond </span>, 
                         <span> Expectations </span>
@@ -17,47 +23,78 @@ const Home = React.createClass({
                         'We are a full-service ad agency that specializes in creating meaningful brand experiences that bring our clients and their customers closer together.',
                         'Using efficient traditional methods enhanced by relevant innovations, we provide effective and smarter services that go beyond what\'s expected in below-the-line advertising.'
                     ],
+                    contextMobile: [
+                        'We are a full-service ad agency that specializes in creating meaningful brand experiences that bring our clients and their customers closer together.',
+                        'Using efficient traditional methods enhanced by relevant innovations, we provide effective and smarter services that go beyond what\'s expected in below-the-line advertising.'
+                    ],
                     contextClass: 'flow-text',
+                    contextClassMobile: 'flow-text',
                     buttonLabel: 'Our Products and Services',
                     buttonName: 'about'
                 },
                 {
                     captionClass: 'col s12 l4 center-align',
                     image: 'img/home/verify.jpg',
+                    imageMobile : 'img/home/verify.jpg',
                     title: [
                         <span className="white-text"> THE FIRST TO HAVE TRULY </span>, 
                         <span className="aai_orange"> VERIFIED </span>, 
                         <span className="white-text"> RESULTS ON-GROUND </span>
                     ],
+                    titleMobile: [
+                        <span className=""> THE FIRST TO HAVE TRULY </span>, 
+                        <span className="aai_orange"> VERIFIED </span>, 
+                        <span className=""> RESULTS ON-GROUND </span>
+                    ],
                     context: [
                         <span className="white-text">We are the first agency to use VERIFY - an on-ground validation and data gathering platform that verifies the actual results and figures collected during on-ground advertising executions.</span>,
                         <span className="white-text">Thru Verify, Activations Advertising gives its clients unmatched transparency in the way it conducts its work on-ground while collecting useful data during activation or sampling runs.</span>,
                     ],
+
+                    contextMobile: [
+                        <span className="">We are the first agency to use VERIFY - an on-ground validation and data gathering platform that verifies the actual results and figures collected during on-ground advertising executions.</span>,
+                        <span className="">Thru Verify, Activations Advertising gives its clients unmatched transparency in the way it conducts its work on-ground while collecting useful data during activation or sampling runs.</span>,
+                    ],
                     contextClass: 'white-text flow-text',
+                    contextClassMobile: 'flow-text',
                     buttonLabel: 'Learn More',
                     buttonName: 'sampling'
                 },
                 {
                     captionClass: 'col s12 l3 right right-align',
                     image: 'img/home/happy_skin.jpg',
+                    imageMobile : 'img/home/happy_skin.jpg',
                     title: [
                         <span className="aai_orange"> HAPPY SKIN </span>, 
                         <span className="white-text"> BRINGS DISNEY&rsquo;S PRINCESSES TO LIFE </span>
                     ],
+                    titleMobile: [
+                        <span className="aai_orange"> HAPPY SKIN </span>, 
+                        <span className=""> BRINGS DISNEY&rsquo;S PRINCESSES TO LIFE </span>
+                    ],
                     context: [],
+                    contextMobile: [],
                     contextClass: 'flow-text',
+                    contextClassMobile: 'flow-text',
                     buttonLabel: 'Read More',
                     buttonName: 'brandActivations'
                 },
                 {
                     captionClass: 'col s12 l4',
                     image: 'img/home/greenwich.jpg',
+                    imageMobile : 'img/home/greenwich.jpg',
                     title: [
                         <span className="aai_orange"> GREENWICH </span>, 
                         <span className="white-text"> CROWNS A NEW ULTIMATE BANDKADA </span>
                     ],
+                    titleMobile: [
+                        <span className="aai_orange"> GREENWICH </span>, 
+                        <span className=""> CROWNS A NEW ULTIMATE BANDKADA </span>
+                    ],
                     context: [],
+                    contextMobile: [],
                     contextClass: 'flow-text white-text',
+                    contextClassMobile: 'flow-text',
                     buttonLabel: 'Read More',
                     buttonName: 'news'
                 }
@@ -73,7 +110,7 @@ const Home = React.createClass({
 
     componentDidMount () {
         $('.slider').slider({
-            height: '800px'
+            height: screen.height
         });
     },
 
@@ -98,7 +135,7 @@ const Home = React.createClass({
                 <img className="leftNavi" onClick={this.handleSliderNavigation} src="img/NAVIGATION/leftArrow.png" />
                 <img className="rightNavi" onClick={this.handleSliderNavigation} src="img/NAVIGATION/rightArrow.png" />
 
-                <div className="slider fullscreen">
+                <div className="slider fullscreen hide-on-med-and-down">
                     <ul className="slides white">
                     { this.state.sliderData.map(function(v,i) {
                         return (
@@ -124,6 +161,52 @@ const Home = React.createClass({
                                                 onClick={this.handleClick.bind(this, v.buttonName)}
                                                 className="waves-effect waves-aai_btn btn z-depth-0"
                                             >{v.buttonLabel}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    }.bind(this))}
+
+                    </ul>
+                </div>
+
+                <div className="slider hide-on-med-and-up"> {/*MOBILE VIEW*/}
+                    <ul className="slides white">
+                    { this.state.sliderData.map(function(v,i) {
+                        return (
+                            <li key={i}>
+                                <div className="caption" style={{top: 0,left: 0,width: '100%'}}>
+                                    <div className="row">
+                                        <div className="card z-depth-0">
+                                            <div className="card-image">
+                                                <img className="responsive-img" src={v.imageMobile} />
+                                                <span className="card-title"></span>
+                                            </div>
+                                            <div className="card-content">
+                                                <center>
+                                                    <h5
+                                                        className="cyan-text text-darken-4"
+                                                    >
+                                                    { v.titleMobile.map(function(vt, it) {
+                                                        return(
+                                                            <span key={it}>{vt}</span>
+                                                        )})
+                                                    }
+                                                    </h5>
+                                                </center>
+                                                { v.contextMobile.map(function(vc, ic) {
+                                                    return(
+                                                        <p key={vc} className={v.contextClassMobile} style={{textAlign : 'justify'}}>{vc}</p>
+                                                    )
+                                                }.bind(this))}
+                                            </div>
+                                            <div className="card-action" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+                                                <a
+                                                    onClick={this.handleClick.bind(this, v.buttonName)}
+                                                    className="waves-effect waves-orange orange accent-3 btn z-depth-0"
+                                                >{v.buttonLabel}</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
